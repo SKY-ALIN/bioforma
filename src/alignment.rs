a90bcb13671ce59e56d9b791d9bbd579e0f2969e
Vladimir Alinsky <vladimir@alinsky.tech>
2023-03-14 11:36:34 +0600

    Add simd_bounded_levenshtein distance

--- src/alignment.rs
+++ src/alignment.rs
@@ -40,12 +40,18 @@ fn simd_levenshtein(alpha: &[u8], beta: &[u8]) -> u32 {
     _simd::levenshtein(alpha, beta)
 }
 
+#[pyfunction]
+fn simd_bounded_levenshtein(alpha: &[u8], beta: &[u8], k: u32) -> Option<u32> {
+    _simd::bounded_levenshtein(alpha, beta, k)
+}
+
 #[pymodule]
 fn distance(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hamming, m)?)?;
     m.add_function(wrap_pyfunction!(simd_hamming, m)?)?;
     m.add_function(wrap_pyfunction!(levenshtein, m)?)?;
     m.add_function(wrap_pyfunction!(simd_levenshtein, m)?)?;
+    m.add_function(wrap_pyfunction!(simd_bounded_levenshtein, m)?)?;
     Ok(())
 }
 
--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -3,13 +3,14 @@ from bioforma.alignment.distance import (
     simd_hamming,
     levenshtein,
     simd_levenshtein,
+    simd_bounded_levenshtein,
 )
 
 
 def test_hamming():
     assert hamming(b'GTCTGCATGCG', b'TTTAGCTAGCG') == 5
     try:
-        hamming(b"GACTATATCGA", b"TTTAGCTC")
+        hamming(b'GACTATATCGA', b'TTTAGCTC')
     except ValueError:
         assert True
     else:
@@ -19,7 +20,7 @@ def test_hamming():
 def test_simd_hamming():
     assert simd_hamming(b'GTCTGCATGCG', b'TTTAGCTAGCG') == 5
     try:
-        simd_hamming(b"GACTATATCGA", b"TTTAGCTC")
+        simd_hamming(b'GACTATATCGA', b'TTTAGCTC')
     except ValueError:
         assert True
     else:
@@ -27,12 +28,27 @@ def test_simd_hamming():
 
 
 def test_levenshtein():
-    assert levenshtein(b"ACCGTGGAT", b"AAAAACCGTTGAT") == 5
-    assert levenshtein(b"ACCGTGGAT", b"AAAAACCGTTGAT") == levenshtein(b"AAAAACCGTTGAT", b"ACCGTGGAT")
-    assert levenshtein(b"AAA", b"TTTT") == 4
+    x = b'ACCGTGGAT'
+    y = b'AAAAACCGTTGAT'
+    assert levenshtein(x, y) == 5
+    assert levenshtein(x, y) == levenshtein(y, x)
+    assert levenshtein(b'AAA', b'TTTT') == 4
 
 
 def test_simd_levenshtein():
-    assert simd_levenshtein(b"ACCGTGGAT", b"AAAAACCGTTGAT") == 5
-    assert simd_levenshtein(b"ACCGTGGAT", b"AAAAACCGTTGAT") == simd_levenshtein(b"AAAAACCGTTGAT", b"ACCGTGGAT")
-    assert simd_levenshtein(b"AAA", b"TTTT") == 4
+    x = b'ACCGTGGAT'
+    y = b'AAAAACCGTTGAT'
+    assert simd_levenshtein(x, y) == 5
+    assert simd_levenshtein(x, y) == simd_levenshtein(y, x)
+    assert simd_levenshtein(b'AAA', b'TTTT') == 4
+
+
+def test_simd_bounded_levenshtein():
+    x = b'ACCGTGGAT'
+    y = b'AAAAACCGTTGAT'
+    max_value = 4_294_967_295
+    assert simd_bounded_levenshtein(x, y, max_value) == 5
+    assert simd_bounded_levenshtein(x, y, max_value) == simd_bounded_levenshtein(y, x, max_value)
+    assert simd_bounded_levenshtein(b'AAA', b'TTTT', max_value) == 4
+    assert simd_bounded_levenshtein(x, y, 5) == 5
+    assert simd_bounded_levenshtein(x, y, 4) is None
